d78f0607e76d8a14db0d7654bc2b969a785ce2e2
Amir Barylko <amir@barylko.com>
2016-09-23 08:38:14 -0700

    Changed mk-board to iterate over parsing until all instructions are parsed

--- src/aoc/day7.clj
+++ src/aoc/day7.clj
@@ -2,22 +2,39 @@
   (:require [clojure.string :as string]
             [clojure.edn :as edn]))
 
+(def empty-board [{} []])
+
+(defn circuit 
+  [[board pending] c]
+  (get board c))
+
 (defn parse
-  [board instruction]
+  [[board pending] instruction]
   (let [[t1 t2 t3 t4 t5] (clojure.string/split instruction #"\ +")
         [v1 v2 v3] (map #(get board %) [t1 t2 t3])
-        [n1 n3] (map read-string [t1 t3])]
-        (cond
-          (= t2 "->"    ) (assoc board t3 n1)
-          (= t2 "AND"   ) (assoc board t5 (bit-and v1 v3))
-          (= t2 "OR"    ) (assoc board t5 (bit-or v1 v3))
-          (= t2 "LSHIFT") (assoc board t5 (bit-shift-left v1 n3))
-          (= t2 "RSHIFT") (assoc board t5 (bit-shift-right v1 n3))
-          (= t1 "NOT"   ) (assoc board t4 (bit-and 0xffff (bit-not v2)))
-          :else board)))
+        [n1 n3] (map read-string [t1 t3])
+        parsed (cond
+                  (= t2 "->"    ) (when n1 (assoc board t3 n1))
+                  (= t2 "AND"   ) (when (and v1 v3) (assoc board t5 (bit-and v1 v3)))
+                  (= t2 "OR"    ) (when (and v1 v3) (assoc board t5 (bit-or v1 v3)))
+                  (= t2 "LSHIFT") (when v1 (assoc board t5 (bit-shift-left v1 n3)))
+                  (= t2 "RSHIFT") (when v1 (assoc board t5 (bit-shift-right v1 n3)))
+                  (= t1 "NOT"   ) (when v2 (assoc board t4 (bit-and 0xffff (bit-not v2)))))]
+          (if parsed [parsed pending] [board (conj pending instruction)])))
+
+
+(defn parse-all
+  [[board instructions]] 
+  (reduce parse [board []] instructions))
+
+(defn still-pending? [[_ pending]] (> (count pending) 0))
 
 (defn mk-board
   [instructions]
-  (reduce parse {} instructions))
+  (->> [{} instructions]
+       (iterate parse-all)
+       (drop-while still-pending?)
+       first
+       first))
 
 
--- test/aoc/day7_test.clj
+++ test/aoc/day7_test.clj
@@ -12,15 +12,39 @@
                     "y RSHIFT 2 -> g"
                     "NOT x -> h"
                     "NOT y -> i"]
-       board (mk-board instructions)]
+       board (parse-all instructions)]
   
-  (is (= 123 (get board "x")))
-  (is (= 456 (get board "y")))
-  (is (= 72 (get board "d")))
-  (is (= 507 (get board "e")))
-  (is (= 492 (get board "f")))
-  (is (= 114 (get board "g")))
-  (is (= 65412 (get board "h")))
-  (is (= 65079 (get board "i")))
-  ))
+  (is (= 123 (circuit board "x")))
+  (is (= 456 (circuit board "y")))
+  (is (= 72 (circuit board "d")))
+  (is (= 507 (circuit board "e")))
+  (is (= 492 (circuit board "f")))
+  (is (= 114 (circuit board "g")))
+  (is (= 65412 (circuit board "h")))
+  (is (= 65079 (circuit board "i")))))
+
+(deftest and-with-number
+  (let [board (parse-all ["456 -> y" "123 AND y -> d"])]
+        (is (= 456 (circuit board "y")))
+        (is (= 72 (circuit board "d")))))
+
+(deftest instructions-no-order
+ (let [instructions ["123 -> x"
+                    "x AND y -> d"
+                    "x OR y -> e"
+                    "x LSHIFT 2 -> f"
+                    "y RSHIFT 2 -> g"
+                    "NOT x -> h"
+                     "456 -> y"
+                    "NOT y -> i"]
+       board (parse-all instructions)]
+  
+  (is (= 123 (circuit board "x")))
+  (is (= 456 (circuit board "y")))
+  (is (= 72 (circuit board "d")))
+  (is (= 507 (circuit board "e")))
+  (is (= 492 (circuit board "f")))
+  (is (= 114 (circuit board "g")))
+  (is (= 65412 (circuit board "h")))
+  (is (= 65079 (circuit board "i")))))
 
