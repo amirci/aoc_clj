9479c91e8d899956e1c29c8b2c2fdf9fd0dd992f
Amir Barylko <amir@barylko.com>
2016-09-23 18:12:35 -0700

    Passed part A

--- src/aoc/day7.clj
+++ src/aoc/day7.clj
@@ -8,35 +8,36 @@
   [[board pending] c]
   (get board c))
 
-(defn parse-int
+(defn try-parse-int
   [s]
   (let [parsed (read-string s)]
        (when (integer? parsed) parsed)))
 
+(def tokenize #(clojure.string/split % #"\ +"))
+
 (defn parse
   [[board pending] instruction]
-  (let [[t1 t2 t3 t4 t5] (clojure.string/split instruction #"\ +")
-        [v1 v2 v3] (map #(or (parse-int %) (get board %)) [t1 t2 t3])
-        [n1 n3] (map read-string [t1 t3])
+  (let [[t1 t2 t3 t4 t5] (tokenize instruction)
+        [v1 v2 v3] (map #(or (try-parse-int %) (get board %)) [t1 t2 t3])
         parsed (cond
-                  (= t2 "->"    ) (when n1 (assoc board t3 n1))
+                  (= t2 "->"    ) (when v1 (assoc board t3 v1))
                   (= t2 "AND"   ) (when (and v1 v3) (assoc board t5 (bit-and v1 v3)))
                   (= t2 "OR"    ) (when (and v1 v3) (assoc board t5 (bit-or v1 v3)))
-                  (= t2 "LSHIFT") (when v1 (assoc board t5 (bit-shift-left v1 n3)))
-                  (= t2 "RSHIFT") (when v1 (assoc board t5 (bit-shift-right v1 n3)))
+                  (= t2 "LSHIFT") (when v1 (assoc board t5 (bit-shift-left v1 v3)))
+                  (= t2 "RSHIFT") (when v1 (assoc board t5 (bit-shift-right v1 v3)))
                   (= t1 "NOT"   ) (when v2 (assoc board t4 (bit-and 0xffff (bit-not v2)))))]
           (if parsed [parsed pending] [board (conj pending instruction)])))
 
 
 (defn parse-all
-  [[board instructions]] 
+  [[board instructions]]
   (let [[new-board new-inst] (reduce parse [board []] instructions)]
        (when (not= (set instructions) (set new-inst))
              [new-board new-inst])))
 
 (defn still-pending? [[_ pending]] (> (count pending) 0))
 
-(defn mk-board
+(defn load-board
   [instructions]
   (->> [{} instructions]
        (iterate parse-all)
--- test/aoc/day7_test.clj
+++ test/aoc/day7_test.clj
@@ -12,31 +12,34 @@
                     "y RSHIFT 2 -> g"
                     "NOT x -> h"
                     "NOT y -> i"]
-       board (mk-board instructions)]
- 
+       board (load-board instructions)]
+
   (is (= {"x" 123 "y" 456 "d" 72  "e" 507 "f" 492 "g" 114 "h" 65412 "i" 65079}
          board))))
 
 (deftest and-with-number
-  (let [board (mk-board ["456 -> y" "123 AND y -> d"])]
+  (let [board (load-board ["456 -> y" "123 AND y -> d"])]
        (is (= {"y" 456 "d" 72} board))))
 
-;(deftest instructions-no-order
-; (let [instructions ["123 -> x"
-;                    "x AND y -> d"
-;                    "x OR y -> e"
-;                    "x LSHIFT 2 -> f"
-;                    "y RSHIFT 2 -> g"
-;                    "NOT x -> h"
-;                     "456 -> y"
-;                    "NOT y -> i"]
-;       board (mk-board instructions)]
-;  (is (= 123 (circuit board "x")))
-;  (is (= 456 (circuit board "y")))
-;  (is (= 72 (circuit board "d")))
-;  (is (= 507 (circuit board "e")))
-;  (is (= 492 (circuit board "f")))
-;  (is (= 114 (circuit board "g")))
-;  (is (= 65412 (circuit board "h")))
-;  (is (= 65079 (circuit board "i")))))
+(deftest assign-circuit-to-circuit
+  (let [board (load-board ["456 -> y" "y -> d"])]
+       (is (= {"y" 456 "d" 456} board))))
+  
+(deftest instructions-no-order
+ (let [instructions ["123 -> x"
+                    "x AND y -> d"
+                    "x OR y -> e"
+                    "x LSHIFT 2 -> f"
+                    "y RSHIFT 2 -> g"
+                    "NOT x -> h"
+                     "456 -> y"
+                    "NOT y -> i"]
+       board (load-board instructions)]
+       (is (= {"x" 123 "y" 456 "d" 72  "e" 507 "f" 492 "g" 114 "h" 65412 "i" 65079}
+              board))))
 
+
+(deftest part-a
+  (let [instructions (clojure.string/split (slurp "resources/day7.input.txt") #"\n")
+        board (load-board instructions)]
+       (is (= 46065 (get board "a")))))
