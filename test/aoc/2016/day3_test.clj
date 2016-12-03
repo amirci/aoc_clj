2eda5b8495cc64c01468c59df60ce7c2b8b241f8
Amir Barylko <amir@barylko.com>
2016-12-03 00:24:53 -0600

    Day 3 part B

--- test/aoc/2016/day3_test.clj
+++ test/aoc/2016/day3_test.clj
@@ -10,9 +10,18 @@
          (map #(clojure.string/split % #"\ +"))
          (map #(map to-int %)))))
 
+(def triangles2
+  (let [part (->> triangles (partition 3))]
+    (->> (for [[[a b c] [d e f] [g h i]] part] [[a d g] [b e h] [c f i]])
+         (apply concat))))
+  
+
 (deftest sample-not-triagle
   (is (not (triangle? [5 10 25]))))
 
-
 (deftest possible-triangles
   (is (= 982 (possible-count triangles))))
+
+(deftest possible-triangles2
+  (is (= 982 (possible-count triangles2))))
+
