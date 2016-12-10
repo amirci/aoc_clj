e46cc3e056c7ce41462b975b8696617b3b39d9ef
Amir Barylko <amir@barylko.com>
2016-12-09 16:12:42 -0800

    Added part B

--- src/aoc/2016/day9.clj
+++ src/aoc/2016/day9.clj
@@ -14,7 +14,25 @@
           code (->> code (drop x))]
       (decode (apply str (concat decoded text expand)) (apply str code)))))
 
+(defn decode2
+  [length code]
+  (when (= 0 (mod (count code) 100000))
+    (trace "===== " [length (count code)]))
+  (if (empty? code)
+    length
+    (let [[match text _ x y] (re-find #"(\w*)(\((\d+)x(\d+)\))?" code)
+          [x y] (if x (map #(Integer. %) [x y]) [0 0])
+          code (drop (count match) code)
+          expand (->> code (take x) (repeat y) (apply concat))
+          code (->> code (drop x))]
+      (recur (+ length (count text)) (apply str (concat expand code))))))
+
 (defn decompress
  [code]
  (decode "" code))
+
+
+(defn decompress2
+ [code]
+ (decode2 0 code))
 
--- test/aoc/2016/day9_test.clj
+++ test/aoc/2016/day9_test.clj
@@ -8,19 +8,32 @@
       clojure.string/split-lines
       first))
 
-(deftest part-a-sample1
-  (is (= 6 (count (decompress "ADVENT")))))
+(def cdec (comp count decompress))
+(def cdec2 (comp count decompress2))
 
-(deftest part-a-sample2
-  (is (= 7 (count (decompress "A(1x5)BC")))))
+;(deftest part-a-sample1
+;  (is (= 6 (count (decompress "ADVENT")))))
+;
+;(deftest part-a-sample2
+;  (is (= 7 (count (decompress "A(1x5)BC")))))
+;
+;(deftest part-a-sample3
+;  (is (= 9 (count (decompress "(3x3)XYZ")))))
+;
+;(deftest part-a-sample4
+;  (is (= 11 (count (decompress "A(2x2)BCD(2x2)EFG")))))
+;
+;; Rank 600
+;(deftest part-a
+;  (is (= 97714 (cdec instructions))))
+;
 
-(deftest part-a-sample3
-  (is (= 9 (count (decompress "(3x3)XYZ")))))
+;(deftest part-b-sample-2
+;  (is (= 1 (cdec2 "X(8x2)(3x3)ABCY"))))
+;
+;(deftest part-b-sample-3
+;  (is (= 241920 (cdec2 "(27x12)(20x12)(13x14)(7x10)(1x12)A"))))
 
-(deftest part-a-sample4
-  (is (= 11 (count (decompress "A(2x2)BCD(2x2)EFG")))))
-
-; Rank 600
-(deftest part-a
-  (is (= 97714 (count (decompress instructions)))))
+(deftest part-b
+  (is (= 1 (cdec2 instructions))))
 
