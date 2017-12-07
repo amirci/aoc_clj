668cae424ef395713d38a5feaf1020e885e9ba9f
Amir Barylko <amir@barylko.com>
2017-12-06 20:49:07 -0800

    Added test for day3

---
+++ test/aoc/2017/day3_test.clj
@@ -0,0 +1,16 @@
+(ns aoc.2017.day3-test
+  (:require [clojure.test :refer :all]
+            [aoc.2017.day3 :refer :all]))
+
+
+
+;(deftest part-b
+;  (->> "resources/2017/day5.input.txt"
+;       slurp
+;       clojure.string/split-lines
+;       count-steps-to-exist-maze-b
+;       (= 23948711)
+;       is))
+;
+;
+;
