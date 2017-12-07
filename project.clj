e549ff9dca9d0f7fb1f5eea856addbf2ba0ce0c2
Amir Barylko <amir@barylko.com>
2017-12-07 12:36:59 -0800

    Finished part A & B
    Added parser for programs
    
    Added branches
    
    Completed empty functions to compile

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [digest "1.4.5"]
                  [org.clojure/math.combinatorics "0.1.3"]
+                 [org.blancas/kern "1.1.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.trace "0.7.9"]])
