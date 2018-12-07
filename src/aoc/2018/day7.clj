ae7c2b81ec8457069ad55315083ad8e86fc54669
Amir Barylko <amir@barylko.com>
2018-12-06 23:01:42 -0800

    Finished part A

--- src/aoc/2018/day7.clj
+++ src/aoc/2018/day7.clj
@@ -3,20 +3,6 @@
     [clojure.string :as str]))
 
 
-(def input
-  (-> "resources/2018/day7.input.txt"
-      slurp
-      clojure.string/split-lines))
-
-(def sample-input
-  ["Step C must be finished before step A can begin."
-   "Step C must be finished before step F can begin."
-   "Step A must be finished before step B can begin."
-   "Step A must be finished before step D can begin."
-   "Step B must be finished before step E can begin."
-   "Step D must be finished before step E can begin."
-   "Step F must be finished before step E can begin."])
-
 (defn parse-line
   [line]
   (let [[_ a _ _ _ _ _ b] (clojure.string/split line #" ")]
---
+++ test/aoc/2018/day7_test.clj
@@ -0,0 +1,26 @@
+(ns aoc.2018.day7-test
+  (:require [clojure.test :refer :all]
+            [aoc.2018.day7 :as dut]))
+
+(def input
+  (-> "resources/2018/day7.input.txt"
+      slurp
+      clojure.string/split-lines))
+
+(def sample-input
+  ["Step C must be finished before step A can begin."
+   "Step C must be finished before step F can begin."
+   "Step A must be finished before step B can begin."
+   "Step A must be finished before step D can begin."
+   "Step B must be finished before step E can begin."
+   "Step D must be finished before step E can begin."
+   "Step F must be finished before step E can begin."])
+
+
+(deftest parta-sample-test
+  (is (= "CABDFE"
+         (dut/part-a sample-input))))
+
+(deftest parta-test
+  (is (= "HPDTNXYLOCGEQSIMABZKRUWVFJ"
+         (dut/part-a input))))
