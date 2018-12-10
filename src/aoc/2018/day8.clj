61a18dc9bd366a2465253c9b02fe061cb2bfb0cf
Amir Barylko <amir@barylko.com>
2018-12-10 01:46:45 -0800

    Finished part B

--- src/aoc/2018/day8.clj
+++ src/aoc/2018/day8.clj
@@ -3,117 +3,12 @@
     [clojure.tools.trace :refer [trace]]
     [clojure.string :as str]))
 
-(defn update-pending-children
-  [pending]
-  (if (seq pending)
-    (let [[chld mtd] (last pending)
-          pending* (pop pending)]
-      (conj pending* [(dec chld) mtd]))
-    pending))
-
-(defn add-pending-parent
-  [pending [chld mtd :as node]]
-  (if (zero? chld)
-    pending
-    (conj pending node)))
-
-(defn add-metadata
-  [mtd total input]
-  [(reduce + total (take mtd input))
-   (drop mtd input)])
-
-(defn check-zero-children
-  [chld mtd total input]
-  (if (zero? chld)
-    (add-metadata mtd total input)
-    [total input]))
-
-
-(defn sum-finished-parents
-  [t p i]
-  (loop [total t pending p input i]
-    (let [[chld mtd] (last pending)
-          [total* input*] (and mtd (add-metadata mtd total input))]
-      (if (and chld (zero? chld))
-        (recur total* (pop pending) input*)
-        [total pending input]))))
-
-(defn read-node
-  [[total pending input]]
-  (let [[chld mtd :as node] (vec (take 2 input))
-        input* (drop 2 input)
-        [total* input*] (check-zero-children chld mtd total input*)
-        pending* (update-pending-children pending)
-        pending* (add-pending-parent pending* node)]
-    (sum-finished-parents total* pending* input*)))
-
-(defn part-a
-  [input]
-  (->> [0 [] input]
-       (iterate read-node)
-       (drop-while (comp seq last))
-       ffirst))
-
-
-;;; Part B
-
-(defn check-leaf-node
-  [{:keys [chld-count mtd-count] :as node} input]
-  (if (zero? chld-count)
-    [(assoc node :metadata (take mtd-count input))
-     (drop mtd-count input)]
-    [node input]))
-
 (defn mk-node
   [input]
   (let [[cc mc] (take 2 input)
         input* (drop 2 input)
         node {:children [] :metadata [] :chld-count cc :mtd-count mc}]
-    (check-leaf-node node input*)))
-
-(defn update-top
-  [pending f]
-  (if (seq pending)
-    (let [node (last pending)]
-      (conj (pop pending) (f node)))
-    pending))
-
-(defn add-pending-parent
-  [pending {:keys [chld-count] :as node}]
-  (if (zero? chld-count)
-    pending
-    (conj pending node)))
-
-(defn reduce-pending-nodes
-  [p i]
-  (loop [pending (pop p)
-         {:keys [chld-count mtd-count] :as top} (last p) 
-         input i]
-    (if (empty? pending)
-      [[top] input]
-      (if (and chld-count (zero? chld-count))
-        (recur (pop pending)
-               (-> top
-                   (update :children conj top)
-                   (assoc :metadata (take mtd-count input)))
-               (drop mtd-count input))
-        [pending input]))))
-
-(defn add-pending-or-to-parent
-  [pending {:keys [chld-count] :as node}]
-  (if (zero? chld-count)
-    (update-top pending #(update % :children conj node))
-    (conj pending node)))
-
-(defn read-tree-node
-  [[pending input]]
-  (let [[node input] (mk-node input)
-        [pending input] (-> pending
-                            (update-top #(update % :chld-count dec))
-                            (add-pending-or-to-parent node)
-                            (reduce-pending-nodes input))]
-
-    [pending input]))
+    [node input*]))
 
 (declare read-tree)
 
@@ -126,13 +21,6 @@
     [[] input]
     (range ct)))
 
-(defn mk-node
-  [input]
-  (let [[cc mc] (take 2 input)
-        input* (drop 2 input)
-        node {:children [] :metadata [] :chld-count cc :mtd-count mc}]
-    [node input*]))
-
 (defn read-tree
   [input]
   (let [[{:keys [chld-count mtd-count] :as node} input] (mk-node input)
@@ -144,16 +32,26 @@
     [(assoc node :children children :metadata metadata)
      input]))
 
-(defn parse-tree
+(defn calc-with-index
+  [{:keys [chld-count children metadata]}]
+  (if (empty? children)
+    (apply + metadata)
+    (let [children (->> metadata
+                        (filter #(<= % chld-count))
+                        (map #(nth children (dec %))))]
+      (->> children
+           (map calc-with-index)
+           (apply +)))))
+
+(defn part-a
   [input]
-  (->> [[] input]
-       (iterate read-tree-node)
-       (drop-while (comp seq second))
-       ffirst))
+  (->> input
+       read-tree
+       first
+       (tree-seq (comp seq :children) :children)
+       (mapcat :metadata)
+       (apply +)))
 
 (defn part-b
   [input]
-  (->> [[] input]
-       (iterate read-tree-node)
-       (drop-while (comp seq second))
-       ffirst))
+  (calc-with-index (first (read-tree input))))
--- test/aoc/2018/day8_test.clj
+++ test/aoc/2018/day8_test.clj
@@ -12,27 +12,6 @@
 
 (def sample-input [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])
 
-; iter 1
-:pending {:chld-count 2 :mtd-count 3 :children []}
-
-; iter 2
-:pending {:chld-count 1 :mtd-count 3
-          :children {:mtd-count 3 :metadata [10 11 12]}}
-
-; iter 4
-:pending {:chld-count 0 :mtd-count 3
-          :children [{:mtd-count 3 :metadata [10 11 12]}
-                     {:mtd-count 1 :chld-count 1 :children []}]}
-
-; iter 5
-:pending {:chld-count 0 :mtd-count 3
-          :children [{:mtd-count 3 :metadata [10 11 12]}
-                     {:mtd-count 0 :chld-count 1 :children [{:metadata [99] :mtd-count 1}]}]}
-
-(def tree {:children [{:metatada [10 11 12] :children []}
-                      {:metadata [2] :children [{:metadata [99]}]}]
-           :metadata [1 1 2]})
-
 (deftest sum-finished-parents-test
   (is (= [138 [] []]
          (dut/sum-finished-parents 132 [[0 3] [0 1]] (drop 0 [2 1 1 2])))))
@@ -46,26 +25,9 @@
 
 (deftest part-b-sample-test
   (is (= 66
+         (dut/part-b sample-input))))
+
+(deftest part-b-test
+  (is (= 37067
          (dut/part-b input))))
 
-*e
-  (->> [[] sample-input]
-       (iterate dut/read-tree-node)
-       (take 5)
-       (map println))
-
-
-(dut/read-tree sample-input)
-(concat (map :metadata (tree-seq (comp seq :children) :children (first (dut/read-tree sample-input)))))
-
-;(map :metadata (tree-seq (comp seq :children) :children (dut/parse-tree sample-input)))
-(dut/parse-tree sample-input)
-;(:metadata (dut/parse-tree sample-input))
-;(:children (dut/parse-tree sample-input))
-;{:children 
-; [{:children [], :metadata (10 11 12), :chld-count 0, :mtd-count 3} 
-;  {:children 
-;   [{:children [], :metadata (99), :chld-count 0, :mtd-count 1} 
-;    {:children [], :metadata (99), :chld-count 0, :mtd-count 1}], 
-;   :metadata (2), :chld-count 0, :mtd-count 1}], 
-; :metadata (1 1 2), :chld-count 0, :mtd-count 3}
