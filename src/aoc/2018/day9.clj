e841de65e1d7172ccfe986f2d876714c8f43939f
Amir Barylko <amir@barylko.com>
2018-12-15 21:00:24 -0800

    Exploring problem

---
+++ src/aoc/2018/day9.clj
@@ -0,0 +1,30 @@
+(ns aoc.2018.day9
+  (:require 
+    [clojure.string :as str]))
+
+
+(defn play-turn
+  [{:keys [scores total next marbles current]|}]
+
+  )
+
+
+; scores  {1 0 2 0  .... 9 0}
+; total   25
+; next    0
+; marbles []
+; current nil
+
+; Player  1
+; scores  (same until 23)
+; total   24
+; next    1
+; marbles [0]
+; current 0
+
+
+; Player 2
+; scores (same)
+; total  24
+; next   2
+
---
+++ test/aoc/2018/day9_test.clj
@@ -0,0 +1,4 @@
+(ns aoc.2018.day9-test
+  (:require [clojure.test :refer :all]
+            [aoc.2018.day9 :as dut]))
+
