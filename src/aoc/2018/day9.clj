0a045a21f49147b74bbf7a8b412df4f4a0ed8b4f
Amir Barylko <amir@barylko.com>
2018-12-16 12:40:26 -0800

    Added implementation until the marble is multiple of 23

--- src/aoc/2018/day9.clj
+++ src/aoc/2018/day9.clj
@@ -2,29 +2,40 @@
   (:require 
     [clojure.string :as str]))
 
+(defn next-player
+  [p players]
+  (if (= p (count players)) 1 (inc p)))
+
+(defn next-index
+  [i marbles]
+  (if (= i (dec (count marbles)))
+    1
+    (+ i 2)))
+
+(def calc-next-marble inc)
+
+(defn insert-marble
+  [circle current marble]
+  (.add circle current marble)
+  circle)
+
+(defn empty-marble-circle [] (insert-marble (java.util.ArrayList.) 0 0))
+
+(defn init-game
+  [total-players total-marbles]
+  {:player 1
+   :scores (->> (range 1 (inc total-players)) (mapv #(vector % 0)) (into {}))
+   :current 0
+   :total total-marbles
+   :marble-circle (empty-marble-circle) 
+   :next-marble 1})
 
 (defn play-turn
-  [{:keys [scores total next marbles current]|}]
-
-  )
-
-
-; scores  {1 0 2 0  .... 9 0}
-; total   25
-; next    0
-; marbles []
-; current nil
-
-; Player  1
-; scores  (same until 23)
-; total   24
-; next    1
-; marbles [0]
-; current 0
-
-
-; Player 2
-; scores (same)
-; total  24
-; next   2
+  [{:keys [player scores total next-marble marble-circle current] :as game}]
+  (let [current (next-index current marble-circle)]
+    (-> game
+        (update :marble-circle insert-marble current next-marble)
+        (assoc  :current       current)
+        (update :player        next-player scores)
+        (update :next-marble   calc-next-marble))))
 
--- test/aoc/2018/day9_test.clj
+++ test/aoc/2018/day9_test.clj
@@ -2,3 +2,47 @@
   (:require [clojure.test :refer :all]
             [aoc.2018.day9 :as dut]))
 
+(defn sample-game [] (dut/init-game 9 25))
+
+(defn the-game [g] (select-keys g [:player :current :next-marble :marble-circle]))
+
+(defn play-some-turns
+  [n]
+  (->> (sample-game)
+       (iterate dut/play-turn)
+       (drop n)
+       first
+       the-game))
+
+;(0) -> sample-game
+
+(deftest sample-players-test
+  (testing "the first few rounds"
+    (is (= [{:player 2 :current 1 :next-marble 2 :marble-circle [0 1]} ;[1]  0 (1)
+            {:player 3 :current 1 :next-marble 3 :marble-circle [0 2 1]} ;[2]  0 (2) 1 
+            {:player 4 :current 3 :next-marble 4 :marble-circle [0 2 1 3]} ;[3]  0  2  1 (3)
+            {:player 5 :current 1 :next-marble 5 :marble-circle [0 4 2 1 3]} ;[4]  0 (4) 2  1  3 
+            {:player 6 :current 3 :next-marble 6 :marble-circle [0 4 2 5 1 3]} ;[5]  0  4  2 (5) 1  3 
+            {:player 7 :current 5 :next-marble 7 :marble-circle [0 4 2 5 1 6 3]} ;[6]  0  4  2  5  1 (6) 3 
+            {:player 8 :current 7 :next-marble 8 :marble-circle [0 4 2 5 1 6 3 7]}   ;[7]  0  4  2  5  1  6  3 (7)
+            {:player 9 :current 1 :next-marble 9 :marble-circle [0 8 4 2 5 1 6 3 7]} ;[8]  0 (8) 4  2  5  1  6  3  7 
+            ]
+           (->> (range 1 9) (map play-some-turns)))))
+
+  (testing "Goes to player one after player 9"
+    (is (= {:player 1 :current 3 :next-marble 10
+            :marble-circle [0 8 4 9 2 5 1 6 3 7]} ;[9]  0  8  4 (9) 2  5  1  6  3  7 
+           (play-some-turns 9))))
+
+  (testing "Playing 22 marbles"
+    (is (= {:player 5 :current 13 :next-marble 23
+            :marble-circle [0 16 8 17 4 18 9 19 2 20 10 21 5 22 11 1 12 6 13 3 14 7 15]} 
+           ; [4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15
+           (play-some-turns 22))))
+
+  (testing "Playing 23 marbles"
+    (is (= {:player 6 :current 6 :next-marble 24
+            :marble-circle [0 16 8 17 4 18 19 2 20 10 21 5 22 11 1 12 6 13 3 14 7 15]} 
+           ;[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15
+           (play-some-turns 23))))
+  )
