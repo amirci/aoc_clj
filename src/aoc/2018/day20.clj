0cd168e266f3907caa9cae889b47a04999225453
Amir Barylko <amir@barylko.com>
2018-12-20 01:01:02 -0800

    Finished parser

--- src/aoc/2018/day20.clj
+++ src/aoc/2018/day20.clj
@@ -5,10 +5,39 @@
     [blancas.kern.lexer.java-style :refer :all]
     [clojure.string :as str]))
 
+; movedir  := one-of NWES
+; move-seq := many1 move-dir
+; list-of-re := sep-end-by1 \| regexp*
+; regexp*  := move-seq (optional (paren list-of-re))
+; regexp   := (sym* ^) regexp* (sym* $)
 
 (def move-dir (one-of "NSWE"))
 
-(value move "E")
+(def move-seq (many1 move-dir))
 
-(^ENWWW(NEEE|SSE(EE|N))$)
+(declare regexp*)
+
+(def list-of-re
+ (bind [re-list (sep-end-by1 (sym* \|) (fwd regexp*))]
+       (return (apply concat re-list))))
+
+(def regexp* 
+  (bind [re move-seq re-list (optional (parens list-of-re))]
+        (let [re-list (case (count re-list)
+                        0 [[]]
+                        1 (conj re-list [])
+                        re-list)
+              re* (for [l re-list] (concat re l))]
+          (return re*))))
+
+(def regexp
+ (bind [_ (sym* \^) re regexp* _ (sym* \$)]
+       (return re)))
+
+;^ENWWW(NEEE|SSE(EE|N))$
+
+(value regexp "^ENWWW$")
+(value regexp "^ENWWW(NEEE|)$")
+(value regexp "^ENWWW(NEEE|SSE)$")
+(value regexp "^ENWWW(NEEE|SSE(EE|N))$")
 
--- test/aoc/2018/day20_test.clj
+++ test/aoc/2018/day20_test.clj
@@ -1,10 +1,17 @@
 (ns aoc.2018.day20-test
-  (:require [clojure.test :refer :all]
-            [aoc.2018.day20 :as dut]))
+  (:require  
+    [clojure.test :refer :all]
+    [blancas.kern.core :as k]
+    [aoc.2018.day20 :as dut]))
 
 
 (def input
   (-> "resources/2018/day20.input.txt"
       slurp))
 
+
+(deftest sample-tests
+  (is (= "N"
+         (k/value dut/regexp "^ENWWW$")
+         )))
 
