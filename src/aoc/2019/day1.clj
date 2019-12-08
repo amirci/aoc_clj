16e162a0c62312465d42e6e6c90fb32cba43a041
Amir Barylko <amir@barylko.com>
2019-12-08 10:36:11 -0800

    Solved part B

--- src/aoc/2019/day1.clj
+++ src/aoc/2019/day1.clj
@@ -3,18 +3,29 @@
    [clojure.string :as str]
    ))
 
-; divide by three, round down, and subtract 2.
 
 (def floor #(java.lang.Math/floorDiv %1 %2))
 
+; divide by three, round down, and subtract 2.
 (defn calc-fuel
   [m]
   (-> m
       (floor 3)
       (- 2)))
 
-(defn modules-total-fuel
-  [modules]
-  (->> modules
-       (map calc-fuel)
+(defn calc-fuel-additional
+  [m]
+  (->> m
+       (iterate calc-fuel)
+       (take-while pos?)
+       (drop 1)
        (apply +)))
+
+(defn modules-total-fuel
+  ([modules] (modules-total-fuel calc-fuel modules))
+  ([f modules]
+   (->> modules
+        (map f)
+        (apply +))))
+
+(def modules-total-fuel-additional (partial modules-total-fuel calc-fuel-additional))
--- test/aoc/2019/day1_test.clj
+++ test/aoc/2019/day1_test.clj
@@ -19,3 +19,16 @@
 (deftest part-a
   (is (= 3266516
          (sut/modules-total-fuel (read-modules)))))
+
+
+;; part B - additional fuel for each calculation
+
+(deftest calc-with-additional-fuel
+  (is (= 2     (sut/calc-fuel-additional 14)))
+  (is (= 966   (sut/calc-fuel-additional 1969)))
+  (is (= 50346 (sut/calc-fuel-additional 100756))))
+
+(deftest part-b
+  (is (= 4896902
+         (sut/modules-total-fuel-additional (read-modules))
+         )))
