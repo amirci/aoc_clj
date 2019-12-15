2330ad8bbd23669a479800ac487c8237369fcba2
Amir Barylko <gbarylko@apple.com>
2019-12-14 20:20:22 -0800

    Finished PART B

--- src/aoc/2019/day5.clj
+++ src/aoc/2019/day5.clj
@@ -8,7 +8,7 @@
 
 (defn next-ptr
   [ptr op]
-  (let [delta (get {\1 4 \2 4 \3 2 \4 2} op 0)]
+  (let [delta (get {\1 4 \2 4 \3 2 \4 2 \5 3 \6 3 \7 4 \8 4} op 0)]
     (+ ptr delta)))
 (def address-mode? (partial = \0))
 
@@ -39,31 +39,60 @@
   (-> (address (:memory runtime) ptr)
       (not= halt-code)))
 
+(def non-zero? (partial not= 0))
+
+(defn jump-if
+  [old-value test? check target]
+  (if (test? check)
+    target
+    old-value))
+
+(defn store-if
+  [memory test? dst p1 p2]
+  (assoc memory
+         dst
+         (if (test? p1 p2) 1 0)))
 
 (defn eval-op 
-  [[op p1 p2 dst m1 m2 m3] {:keys [input memory] :as runtime}]
+  [[op p1 p2 dst m1 m2 m3] {{:keys [input memory]} :runtime :as program}]
   (let [v1 #(read-moded-param memory p1 m1)
         v2 #(read-moded-param memory p2 m2)]
     (case op
-      \1 (update runtime :memory assoc dst (+ (v1) (v2)))
-      \2 (update runtime :memory assoc dst (* (v1) (v2)))
-      \3 (update runtime :memory assoc p1 input)
-      \4 (update runtime :outputs conj (v1)))))
+      \1 (update-in program [:runtime :memory] assoc dst (+ (v1) (v2)))
+      \2 (update-in program [:runtime :memory] assoc dst (* (v1) (v2)))
+      \3 (update-in program [:runtime :memory] assoc p1 input)
+      \4 (update-in program [:runtime :outputs] conj (v1))
+      \5 (update-in program [:ptr] jump-if non-zero? (v1) (v2))
+      \6 (update-in program [:ptr] jump-if zero? (v1) (v2))
+      \7 (update-in program [:runtime :memory] store-if < dst (v1) (v2))
+      \8 (update-in program [:runtime :memory] store-if = dst (v1) (v2))
+      )))
+
+(defn update-ptr-if-wasnt-updated
+  [op new-ptr old-ptr {:keys [ptr] :as program}]
+  (if (= ptr old-ptr)
+    (assoc program :ptr new-ptr)
+    program))
 
 (defn run-instruction
-  [program]
-  (let [[op next] (parse-op program)]
-    (-> program
-        (assoc :ptr next)
-        (update :runtime (partial eval-op op)))))
+  [{old-ptr :ptr :as program }]
+  (let [[op next-ptr] (parse-op program)]
+    (->> program
+         (eval-op op)
+         (update-ptr-if-wasnt-updated op next-ptr old-ptr)
+        )))
 
+(defn ->instruction
+  [code input]
+  {:ptr 0 :runtime {:memory code :input input :outputs []}})
 
 (defn run-program
-  [code]
-  (->> {:ptr 0 :runtime {:memory code :input 1 :outputs []}}
+  ([code] (run-program code 1))
+  ([code input]
+  (->> (->instruction code input)
        (iterate run-instruction)
        (drop-while keep-eval?)
-       first))
+       first)))
 
 (defn run-program-last-ouput
   [code]
--- test/aoc/2019/day5_test.clj
+++ test/aoc/2019/day5_test.clj
@@ -1,6 +1,11 @@
 (ns aoc.2019.day5-test
   (:require
    [aoc.2019.day5 :as sut]
+   [clojure.test :refer [testing]]
+   [clojure.test.check :as tc]
+   [clojure.test.check.clojure-test :refer [defspec]]
+   [clojure.test.check.generators :as g]
+   [clojure.test.check.properties :as prop]
    [clojure.java.io :as io]
    [clojure.edn :as edn]
    [clojure.test :as t :refer [deftest is]]))
@@ -37,10 +42,10 @@
 
 (deftest running-instruction-test
   (is (= {:input nil :outputs [] :memory [0 1 2 3 4 5 6 7 8 9 88 11 12]}
-         (sut/run-instruction {:ptr 0 :runtime {:memory  [1002,4,3,4,2] :input nil :outputs [] }})))
+         (sut/run-instruction {:ptr 0 :runtime {:memory  [1002,4,3,4,2] :input nil :outputs [] }})
+         ))
   (is (= 1
-         (sut/run-instruction {:ptr 2 :runtime {:memory (assoc intcode 225 1) :input 1 :outputs []}})
-         )))
+         (sut/run-instruction {:ptr 2 :runtime {:memory (assoc intcode 225 1) :input 1 :outputs []}}))))
 
 (def program-3 
   (->> {:ptr 0 :runtime {:memory intcode :input 1 :outputs []}}
@@ -51,5 +56,48 @@
 (deftest part-a-test
   (is (= 15508323
          (sut/run-program-last-ouput intcode))))
+
+
+(def part-b-samples
+  [
+;   [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9] 5 1
+   ])
+
+(defn run-it
+  [code input]
+  (get-in
+    (sut/run-program code input)
+    [:runtime :outputs]))
+
+(defspec part-b-sample-1
+  100
+  (prop/for-all [[input expected] (g/let [i g/pos-int] [i (if (= i 8) 1 0)])]
+                 (is (= [expected] (run-it [3,9,8,9,10,9,4,9,99,-1,8] input)))))
+
+(defspec part-b-sample-2
+  100
+  (prop/for-all [[input expected] (g/let [i g/pos-int] [i (if (< i 8) 1 0)])]
+                (is (= [expected] (run-it [3,9,7,9,10,9,4,9,99,-1,8] input)))))
+
+(defspec part-b-sample-3
+  100
+  (prop/for-all [[input expected] (g/let [i g/pos-int] [i (if (zero? i) 0 1)])]
+                (is (= [expected] (run-it [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9] input)))))
+
+(def sample-4
+  [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
+
+(defspec part-b-sample-4
+  100
+  (prop/for-all [[input expected] (g/let [i g/pos-int] [i (cond
+                                                            (< i 8) 999
+                                                            (= 8 i) 1000
+                                                            (> i 8) 1001)])]
+                (is (= [expected] (run-it sample-4 input)))))
+
+
+(deftest part-b-test
+  (is (= [9006327]
+         (get-in (sut/run-program intcode 5) [:runtime :outputs]))))
 
 
