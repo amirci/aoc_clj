c0c83b39790eac1d7fe7f9b9ece642bf0c55861b
Amir Barylko <amir@barylko.com>
2020-01-05 22:34:44 -0800

    Removed failing test (not needed)

--- test/aoc/2018/day8_test.clj
+++ test/aoc/2018/day8_test.clj
@@ -12,10 +12,6 @@
 
 (def sample-input [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])
 
-(deftest sum-finished-parents-test
-  (is (= [138 [] []]
-         (dut/sum-finished-parents 132 [[0 3] [0 1]] (drop 0 [2 1 1 2])))))
-
 (deftest part-a-sample-test
   (is (= 138 (dut/part-a sample-input))))
 
