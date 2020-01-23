ef57cdd364413536d7e31209c8da7baba318cb43
Amir Barylko <amir@barylko.com>
2020-01-23 08:39:35 -0800

    Started to calculate gravity and velocity

--- src/aoc/2019/day12.clj
+++ src/aoc/2019/day12.clj
@@ -5,12 +5,25 @@
   [pos vel]
   {:pos pos :vel vel})
 
+(defn apply-gravity
+  [planets])
+
+(defn apply-velocity
+  [planets]
+  (for [planet planets :let [rst (disj planets planet)]]
+    (reduce (fn [planet other]
+              )
+            planet
+            rst)))
+
 (defn time-step
   [planets]
-  planets)
+  (apply-velocity
+   (apply-gravity planets)))
 
 (defn moon-time-steps
   [planets]
   (->> planets
        (map #(mk-planet % [0 0 0]))
+       set
        (iterate time-step)))
--- test/aoc/2019/day12_test.clj
+++ test/aoc/2019/day12_test.clj
@@ -9,11 +9,16 @@
    [11, 11, -4]])
 
 (def sample-planets
-  [[-1 0 2   ]
-   [2 -10 -7 ]
-   [4 -8 8   ]
-   [3  5 -1   ]])
+  [[-1 0  2 ]
+   [2 -10 -7]
+   [4 -8  8 ]
+   [3  5  -1]])
 
 (deftest time-steps-test
   (is (= (map #(sut/mk-planet % [0 0 0]) sample-planets)
-         (first (sut/moon-time-steps sample-planets)))))
+         (first (sut/moon-time-steps sample-planets))))
+  (is (= [(sut/mk-planet [ 2, -1,  1 ] [  3, -1, -1 ])
+          (sut/mk-planet [ 3, -7, -4 ] [  1,  3,  3 ])
+          (sut/mk-planet [ 1, -7,  5 ] [ -3,  1, -3 ])
+          (sut/mk-planet [ 2,  2,  0 ] [ -1, -3,  1 ])]
+         (first (drop 1 (sut/moon-time-steps sample-planets))))) )
