2960b739e14bd9e7b5d4547412639b968e1715ca
Amir Barylko <amir@barylko.com>
2024-02-01 22:23:12 -0800

    Starting part 1

---
+++ test/aoc/2023/day21_test.clj
@@ -0,0 +1,25 @@
+(ns aoc.2023.day21-test
+  (:require [aoc.2023.day21 :as sut]
+            [clojure.string :as s]
+            [clojure.test :refer [deftest is] :as t]))
+
+(def input
+  (->> "resources/2023/day21.txt"
+       slurp
+       s/split-lines))
+
+(def sample
+  ["..........."
+   ".....###.#."
+   ".###.##..#."
+   "..#.#...#.."
+   "....#.#...."
+   ".##..S####."
+   ".##..#...#."
+   ".......##.."
+   ".##.#.####."
+   ".##..##.##."
+   "..........."])
+
+(deftest part1-test
+  (is (= 1 (sut/your-function-here sample))))
